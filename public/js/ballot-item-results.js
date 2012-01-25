1dddd1640b0255b628520c9878c7019a07d95d40
Bradley Holt <bradley.holt@foundline.com>
2012-01-25 10:57:41 -0500

    Remove old TODO comment.

--- public/js/ballot-item-results.js
+++ public/js/ballot-item-results.js
@@ -22,7 +22,6 @@ $(document).ready(function() {
             }
             //TODO: Default refresh time?
             updateBallotItemArticle(uri, $(element), 60000, cache);
-            //TODO: Indicate when results were udpated for each item, e.g. "Results updated within the last five minutes."
         });
     }
 
